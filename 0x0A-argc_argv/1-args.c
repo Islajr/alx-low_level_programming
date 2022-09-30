c9c883bb626eb9c499a323d42ce25392bed6fbb9
Isaac <isaacakinmokun@gmail.com>
2022-09-30 10:50:00 -0700

    At long last!

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 /**
@@ -15,22 +14,3 @@ int main(int argc, char *argv[])
 	printf("%d\n", argc - 1);
 	return (0);
 }
-
-=======
-#include <stdio.h>
-
-/**
- * main - prints the number of arguments passed into it
- * @argc: arguments counter
- * @argv: arguments pointer to array
- * Return: 0 - success.
- */
-
-int main(int argc, char *argv[])
-{
-	(void) argv;
-
-	printf("%d\n", argc - 1);
-	return (0);
-}
->>>>>>> e0059a7bd0d06a009e7e7fa693bb131e76bd1725
