1209a7b76cf0fbde94839c0f4bac9662a1daead1
Isaac <isaacakinmokun@gmail.com>
2022-09-30 10:07:41 -0700

    Failed, innit?

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - prints all arguments it receives
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0 - success
  */
+
 int main(int argc, char *argv[])
 {
 	int i;
@@ -16,6 +15,6 @@ int main(int argc, char *argv[])
 	{
 		printf("%s\n", argv[i]);
 	}
-
 	return (0);
 }
+
